f56e2545b49716bdefbf6a3ef0fd0fc27f66e852
FinestGit <chaserogness@gmail.com>
2025-07-17 10:25:39 -0500

    More changes

--- src/main/java/com/github/finestgit/adventurecraft/datagen/ModRecipeProvider.java
+++ src/main/java/com/github/finestgit/adventurecraft/datagen/ModRecipeProvider.java
@@ -3,10 +3,7 @@ package com.github.finestgit.adventurecraft.datagen;
 import com.github.finestgit.adventurecraft.item.ModItems;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
-import net.minecraft.data.recipes.RecipeCategory;
-import net.minecraft.data.recipes.RecipeOutput;
-import net.minecraft.data.recipes.RecipeProvider;
-import net.minecraft.data.recipes.ShapedRecipeBuilder;
+import net.minecraft.data.recipes.*;
 import net.minecraft.world.item.Items;
 import net.neoforged.neoforge.common.conditions.IConditionBuilder;
 
--- src/main/java/com/github/finestgit/adventurecraft/item/woodcutting/WoodcuttingAxeItem.java
+++ src/main/java/com/github/finestgit/adventurecraft/item/woodcutting/WoodcuttingAxeItem.java
@@ -3,6 +3,9 @@ package com.github.finestgit.adventurecraft.item.woodcutting;
 import com.github.finestgit.adventurecraft.AdventureCraftMod;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
@@ -12,14 +15,23 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 
 import java.util.List;
+import java.util.Random;
 
 public class WoodcuttingAxeItem extends Item {
+    int maxDurability = 60;
+    int minDurability = 40;
+    int durability = 0;
     public WoodcuttingAxeItem(Properties properties) {
         super(properties);
     }
 
     public void assignStats() {
         AdventureCraftMod.LOGGER.debug("Creating Stats");
+    }
+
+    @Override
+    public boolean isDamageable(ItemStack stack) {
+        return true;
     }
 
     @Override
@@ -32,12 +44,19 @@ public class WoodcuttingAxeItem extends Item {
 
     @Override
     public boolean mineBlock(ItemStack stack, Level level, BlockState state, BlockPos pos, LivingEntity miningEntity) {
+        if (!level.isClientSide()) {
+            Player player = ((Player) miningEntity);
+            player.getItemInHand(InteractionHand.MAIN_HAND).hurtAndBreak(1, ((ServerLevel) level), player,
+                    item -> player.onEquippedItemBroken(item, EquipmentSlot.MAINHAND));
+        }
         return super.mineBlock(stack, level, state, pos, miningEntity);
     }
 
     @Override
     public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
         tooltipComponents.add(Component.literal("Some cool text"));
+        int currentDurability = stack.getMaxDamage() - stack.getDamageValue();
+        tooltipComponents.add(Component.literal("Durability: " + currentDurability + "/" + stack.getMaxDamage()));
         super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
     }
 }
